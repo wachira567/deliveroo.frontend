09937fd230ee7fff2ddae7418a190c21d20646e7
+ <+>
2026-02-14 20:45:28 +0300

    fix(frontend): remove default content-type to support multipart uploads

--- src/services/api.js
+++ src/services/api.js
@@ -5,9 +5,6 @@ const API_BASE_URL =
 
 const api = axios.create({
   baseURL: API_BASE_URL,
-  headers: {
-    "Content-Type": "application/json",
-  },
 });
 
 // Request interceptor to add auth token
